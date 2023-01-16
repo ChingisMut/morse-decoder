a932b66291f16ce9fb708f35cc219a0c7fd46639
ChingisMut <mchp94@mail.ru>
2023-01-17 01:06:05 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let num = expr.split('');
+    let one = [];
+    let three = [];
+    for (let i = 0; i <num.length; i++) {
+        if (i % 2 !== 0) {
+            one.push(num[i] + ' ');
+        } else {
+            one.push(num[i]);
+        }
+    }
+    let two = one.join('').split(' ');
+    for (let j = 0; j < two.length; j++) {
+        if (j % 5 === 0) {
+            three.push('1')
+            if(two[j] === '10') {
+                three.push('.');
+            } if (two[j] === '11') {
+                three.push('-');
+            } if (two[j] === '**') {
+                three.push('2');
+            } if(two[j] === '00') {
+                continue
+            }
+        } else {
+            if(two[j] === '10') {
+                three.push('.');
+            } if (two[j] === '11') {
+                three.push('-');
+            } if (two[j] === '**') {
+                three.push('2');
+            } if(two[j] === '00') {
+                continue
+            }
+        }
+    }
+    let four = three.join('').split('1');
+    let five = [];
+    for (let k = 0; k < four.length; k++) {
+        five.push(MORSE_TABLE[four[k]]);
+    }
+    five.shift();
+    let six = five.join('');
+    return six;
 }
 
 module.exports = {
